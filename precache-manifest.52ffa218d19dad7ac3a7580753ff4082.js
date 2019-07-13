131e5bf3c5df51000979cacf6e52802e60089899
Mike Huitrado <huitrado.mike@gmail.com>
2019-07-13 19:51:07 -0300

    Updates

---
+++ precache-manifest.52ffa218d19dad7ac3a7580753ff4082.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647c1a213a9e7b0143dbca8d06fafce1",
+    "url": "/rocketshoes-react-js/index.html"
+  },
+  {
+    "revision": "a2e132eb3ce72ff09b02",
+    "url": "/rocketshoes-react-js/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "a2e132eb3ce72ff09b02",
+    "url": "/rocketshoes-react-js/static/js/2.890f431f.chunk.js"
+  },
+  {
+    "revision": "bbeb8163c55175b73916",
+    "url": "/rocketshoes-react-js/static/js/main.75e8a3d5.chunk.js"
+  },
+  {
+    "revision": "83de94ec6aba1f0ae857",
+    "url": "/rocketshoes-react-js/static/js/runtime~main.962d3050.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/rocketshoes-react-js/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/rocketshoes-react-js/static/media/logo.879557c5.svg"
+  }
+]);
