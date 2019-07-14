c5be9565c84e851ee201a66126d8b3cad92052d0
Mike Huitrado <huitrado.mike@gmail.com>
2019-07-14 09:32:05 -0300

    UPDATE: remove unused imports on Cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   MdAddCircleOutline,
   MdRemoveCircleOutline,
